7855736015f71e304cb7717ca9c94745c5d1f604
Igor Fiori <150206681+igorfiori@users.noreply.github.com>
2024-07-04 22:43:08 -0300

    Update Apartamento.java

--- src/modelo/Apartamento.java
+++ src/modelo/Apartamento.java
@@ -1,2 +1,36 @@
-package modelo;public class Apartamento {
+package modelo;
+
+public class Apartamento extends Financiamento {
+    private int numeroVagasGaragem;
+    private int numeroAndar;
+
+    public Apartamento(double valorImovel, int prazoFinanciamento, double taxaJurosAnual, int numeroVagasGaragemApt2, int numeroAndarApt2) {
+        super(valorImovel, prazoFinanciamento, taxaJurosAnual);
+        this.numeroVagasGaragem = numeroVagasGaragem;
+        this.numeroAndar = numeroAndar;
+    }
+
+    public int getNumeroVagasGaragem() {
+        return numeroVagasGaragem;
+    }
+
+    public void setNumeroVagasGaragem(int numeroVagasGaragem) {
+        this.numeroVagasGaragem = numeroVagasGaragem;
+    }
+
+    public int getNumeroAndar() {
+        return numeroAndar;
+    }
+
+    public void setNumeroAndar(int numeroAndar) {
+        this.numeroAndar = numeroAndar;
+    }
+
+    @Override
+    public double calcularPagamentoMensal() {
+        double taxaMensal = taxaJurosAnual / 12.0 / 100.0;
+        int meses = prazoFinanciamento * 12;
+        double pagamentoMensal = valorImovel * taxaMensal * Math.pow(1 + taxaMensal, meses) / (Math.pow(1 + taxaMensal, meses) - 1);
+        return pagamentoMensal;
+    }
 }
