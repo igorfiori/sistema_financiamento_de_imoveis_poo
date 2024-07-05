1f32623a678bd5db416ba19fcadd4099c27ad1d6
Igor Fiori <150206681+igorfiori@users.noreply.github.com>
2024-07-04 22:45:28 -0300

    Update InterfaceUsuario.java

--- src/util/InterfaceUsuario.java
+++ src/util/InterfaceUsuario.java
@@ -1,3 +1,6 @@
+package util;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InterfaceUsuario {
@@ -8,43 +11,180 @@ public class InterfaceUsuario {
         this.scanner = new Scanner(System.in);
     }
 
-    // Método para solicitar ao usuário o valor do imóvel
-    public double pedirValorImovel() {
-        double valor;
+    // Método para solicitar ao usuário o tipo de imóvel desejado
+    public String pedirTipoImovel() {
+        String tipoImovel = "";
+        boolean inputValido = false;
         do {
-            System.out.print("Digite o valor do imóvel (deve ser positivo): ");
-            valor = scanner.nextDouble();
-            if (valor <= 0) {
-                System.out.println("Valor inválido. O valor do imóvel deve ser positivo.");
+            try {
+                System.out.print("Digite o tipo de imóvel desejado (casa, apartamento ou terreno): ");
+                tipoImovel = scanner.nextLine().toLowerCase();
+                if (!tipoImovel.equals("casa") && !tipoImovel.equals("apartamento") && !tipoImovel.equals("terreno")) {
+                    throw new IllegalArgumentException("Tipo de imóvel inválido. Por favor, digite 'casa', 'apartamento' ou 'terreno'.");
+                }
+                inputValido = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
-        } while (valor <= 0);
+        } while (!inputValido);
+        return tipoImovel;
+    }
+
+    // Métodos para solicitar os dados comuns de financiamento
+    public double pedirValorImovel() {
+        double valor = 0;
+        boolean inputValido = false;
+        do {
+            try {
+                System.out.print("Digite o valor do imóvel (entre R$250.000,00 e R$5.000.000,00): ");
+                valor = scanner.nextDouble();
+                if (valor < 250000 || valor > 5000000) {
+                    throw new IllegalArgumentException("Valor inválido. O valor do imóvel deve estar entre R$250.000,00 e R$5.000.000,00.");
+                }
+                inputValido = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Por favor, digite um número.");
+                scanner.nextLine(); // Limpa o buffer do scanner
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!inputValido);
         return valor;
     }
 
-    // Método para solicitar ao usuário o prazo do financiamento em anos
     public int pedirPrazoFinanciamento() {
-        int prazo;
+        int prazo = 0;
+        boolean inputValido = false;
         do {
-            System.out.print("Digite o prazo do financiamento em anos (deve ser positivo): ");
-            prazo = scanner.nextInt();
-            if (prazo <= 0) {
-                System.out.println("Valor inválido. O prazo do financiamento deve ser positivo.");
+            try {
+                System.out.print("Digite o prazo do financiamento em anos (entre 2 e 25 anos): ");
+                prazo = scanner.nextInt();
+                if (prazo < 2 || prazo > 25) {
+                    throw new IllegalArgumentException("Valor inválido. O prazo do financiamento deve estar entre 2 e 25 anos.");
+                }
+                inputValido = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Por favor, digite um número inteiro.");
+                scanner.nextLine(); // Limpa o buffer do scanner
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
-        } while (prazo <= 0);
+        } while (!inputValido);
         return prazo;
     }
 
-    // Método para solicitar ao usuário a taxa de juros anual
     public double pedirTaxaJuros() {
-        double taxa;
+        double taxa = 0;
+        boolean inputValido = false;
         do {
-            System.out.print("Digite a taxa de juros anual (%) (deve ser positiva): ");
-            taxa = scanner.nextDouble();
-            if (taxa <= 0) {
-                System.out.println("Valor inválido. A taxa de juros anual deve ser positiva.");
+            try {
+                System.out.print("Digite a taxa de juros anual (% entre 11% e 12,2%): ");
+                taxa = scanner.nextDouble();
+                if (taxa < 11 || taxa > 12.2) {
+                    throw new IllegalArgumentException("Valor inválido. A taxa de juros anual deve estar entre 11% e 12.2%.");
+                }
+                inputValido = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Por favor, digite um número.");
+                scanner.nextLine(); // Limpa o buffer do scanner
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
-        } while (taxa <= 0);
+        } while (!inputValido);
         return taxa;
+    }
+
+    // Métodos adicionais para solicitar os dados específicos de cada tipo de imóvel
+    public double pedirAreaConstruida() {
+        double areaConstruida = 0;
+        boolean inputValido = false;
+        do {
+            try {
+                System.out.print("Digite a área construída M² do imóvel: ");
+                areaConstruida = scanner.nextDouble();
+                inputValido = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Por favor, digite um número.");
+                scanner.nextLine(); // Limpa o buffer do scanner
+            }
+        } while (!inputValido);
+        return areaConstruida;
+    }
+
+    public double pedirTamanhoTerreno() {
+        double tamanhoTerreno = 0;
+        boolean inputValido = false;
+        do {
+            try {
+                System.out.print("Digite o tamanho do terreno M² do imóvel: ");
+                tamanhoTerreno = scanner.nextDouble();
+                inputValido = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Por favor, digite um número.");
+                scanner.nextLine(); // Limpa o buffer do scanner
+            }
+        } while (!inputValido);
+        return tamanhoTerreno;
+    }
+
+    public int pedirNumeroVagasGaragem() {
+        int numeroVagas = 0;
+        boolean inputValido = false;
+        do {
+            try {
+                System.out.print("Digite o número de vagas na garagem do apartamento (entre 0 e 7): ");
+                numeroVagas = scanner.nextInt();
+                if (numeroVagas < 0 || numeroVagas > 7) {
+                    throw new IllegalArgumentException("Número de vagas inválido. Por favor, digite um valor entre 0 e 7.");
+                }
+                inputValido = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Por favor, digite um número inteiro.");
+                scanner.nextLine(); // Limpa o buffer do scanner
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!inputValido);
+        return numeroVagas;
+    }
+
+    public int pedirNumeroAndar() {
+        int numeroAndar = 0;
+        boolean inputValido = false;
+        do {
+            try {
+                System.out.print("Digite o número do andar do apartamento (-1 para subsolo, 1 a 128 para andares): ");
+                numeroAndar = scanner.nextInt();
+                if (numeroAndar < -1 || numeroAndar > 128) {
+                    throw new IllegalArgumentException("Número de andares inválido. Por favor, digite um valor entre -1 e 128.");
+                }
+                inputValido = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Por favor, digite um número inteiro.");
+                scanner.nextLine(); // Limpa o buffer do scanner
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!inputValido);
+        return numeroAndar;
+    }
+
+    public String pedirTipoZona() {
+        String tipoZona = "";
+        boolean inputValido = false;
+        do {
+            try {
+                System.out.print("Digite o tipo de zona do terreno (residencial ou comercial): ");
+                tipoZona = scanner.next().toLowerCase();
+                if (!tipoZona.equals("residencial") && !tipoZona.equals("comercial")) {
+                    throw new IllegalArgumentException("Tipo de zona inválido. Por favor, digite 'residencial' ou 'comercial'.");
+                }
+                inputValido = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!inputValido);
+        return tipoZona;
     }
 
     // Método para fechar o scanner
